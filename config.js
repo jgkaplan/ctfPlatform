fb4e3600a3aa99134e44f0eaf4a56ff70d6ea3d1
jgk83 <jgk83@cornell.edu>
2018-05-26 13:39:33 -0500

    Began adding easter egg support. Sorted teams on scoreboard

--- config.js
+++ config.js
@@ -2,16 +2,17 @@ const path = require('path');
 module.exports = {
     "webPort": 3000,
     "webPortSecure": 3001,
-    "sessionSecret": "fklajsdlbkjLAEO31B",
-    "useHTTP": true,
-    "useHTTPS": false,
-    "httpsKeyFile": "path/to/key/file",
-    "httpsCertFile": "path/to/cert/file",
+    "sessionSecret": "fklajsdlbkjLAEO31B", /* change me */
+    "useHTTPS": true,
+    "httpsKeyFile": path.join(__dirname,"server","localhost-privkey.pem"), /* path/to/key/file */
+    "httpsCertFile": path.join(__dirname,"server","localhost-cert.pem"), /* path/to/cert/file */
     "dbLocation": "mongodb://localhost/ctfPlatform",
     "competitionStart": -1, // use new Date(year, month (0 indexed), day, hour, minute, second) or -1
     "competitionEnd": -1, // use new Date(year, month, day, hour, minute, second) or -1
     "maxTeamSize": 4,
     "problemDir": path.join(__dirname, 'problems'),
     "backupDir": path.join(__dirname, 'backups'),
-    "adminAccountNames": ["Admin", "admin"]
+    "adminAccountNames": ["Admin", "admin"],
+    "restrictedNames": ["admin","chc_"],
+    "easterEggUrl": "eastereggs"
 }
